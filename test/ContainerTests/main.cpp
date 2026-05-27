aa624afd185d23291f5ca5bb891b28b5784fe98e
Avelina9X <luke.kyriacou.9x@gmail.com>
2026-05-27 22:37:55 +0100

    Added doctest and primitive test main

---
+++ test/ContainerTests/main.cpp
@@ -0,0 +1,21 @@
+#define _CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
+
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "doctest.h"
+
+int main( int argc, char **argv )
+{
+	_CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
+	_CrtSetReportMode( _CRT_WARN, _CRTDBG_MODE_DEBUG );
+
+	doctest::Context context;
+
+	context.applyCommandLine( argc, argv );
+
+	int res = context.run();
+
+	if ( context.shouldExit() ) return res;
+
+	return res;
+}
